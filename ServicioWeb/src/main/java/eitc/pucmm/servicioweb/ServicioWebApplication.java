ff711056b4e9b1712ff08212181d0be39712c93a
Felix <felix1020304@gmail.com>
2022-06-05 14:53:55 -0400

    Inicializacion de servicio web

---
+++ ServicioWeb/src/main/java/eitc/pucmm/servicioweb/ServicioWebApplication.java
@@ -0,0 +1,13 @@
+package eitc.pucmm.servicioweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicioWebApplication.class, args);
+    }
+
+}
---
+++ ServicioWeb/src/test/java/eitc/pucmm/servicioweb/ServicioWebApplicationTests.java
@@ -0,0 +1,13 @@
+package eitc.pucmm.servicioweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
