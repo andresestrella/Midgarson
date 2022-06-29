2e09c6cbac25babfd7e297ab0c7987ff03e76147
lendry02 <marrero0812@gmail.com>
2022-06-29 19:59:42 -0400

    Prefab de fin de juego hecho

--- Midgarson/Assets/Scripts/PlayerMovement.cs
+++ Midgarson/Assets/Scripts/PlayerMovement.cs
@@ -30,6 +30,8 @@ public class PlayerMovement : MonoBehaviour
 
     private Animator anim;
 
+    public SceneController sceneController;
+
     private void Awake()
     {
         myBody = GetComponent<Rigidbody2D>();
@@ -209,8 +211,13 @@ public class PlayerMovement : MonoBehaviour
             anim.SetBool(FALL_ANIMATION, false);
             myBody.constraints = RigidbodyConstraints2D.FreezeRotation;
         }
+        else if (collision.gameObject.tag == "END")
+        {
+            sceneController.LevelClear();
 
-        
+        }
+
+
     }
 
     void OnCollisionExit2D(Collision2D collision){
