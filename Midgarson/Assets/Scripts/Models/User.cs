a3678f0f033a3e5a3710012b004c130fc4d98495
Felix <felix1020304@gmail.com>
2022-07-04 09:04:21 -0400

    Auto stash before merge of "main" and "origin/main"

---
+++ Midgarson/Assets/Scripts/Models/User.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class User
+{
+    public long id;
+    public string name;
+    public string password;
+    public long puntaje;
+}
---
+++ Midgarson/Assets/Scripts/bombBehaviour.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bombBehaviour : MonoBehaviour
+{
+    Vector3 currentSpeed = new Vector3();
+    Vector3 _deltaPos = new Vector3();
+    int damage = 40;
+    float damageRange = 10f;
+    public LayerMask enemyLayers;
+    string explode;
+    bool _shooted;
+    private Animator anim;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_shooted)
+            return;
+        _deltaPos = currentSpeed * Time.deltaTime + Physics.gravity * Mathf.Pow(Time.deltaTime, 2) / 2;
+        gameObject.transform.Translate(_deltaPos);
+        currentSpeed += Physics.gravity * Time.deltaTime;
+    }
+
+    public void Shoot(Vector3 startingSpeed, float shootingAngle)
+    {
+        currentSpeed = new Vector3(startingSpeed.x * Mathf.Cos(shootingAngle), startingSpeed.y * Mathf.Sin(shootingAngle));
+        _shooted = true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        anim.SetBool(explode, true);
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(gameObject.transform.position, damageRange, enemyLayers);
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("We hit " + enemy.name);
+            enemy.GetComponent<BasicEnemy>().takeDamage(damage);
+        }
+        Destroy(gameObject);
+    }
+    
+}
