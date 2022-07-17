4ac6b70fd0eb9f7ca8d2369eed7221b579d34f10
Kal567 <kianaalmonte21@gmail.com>
2022-07-17 14:16:42 -0400

    Add - level selection scene

---
+++ Midgarson/Assets/Scripts/LevelSelection/CameraMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public static int direction = 0;
+
+    Vector3 _deltaPos = new Vector3();
+    Vector3 _movementSpeed = new Vector3(5,0);
+    const float MIN_X = 0f, MAX_X = 19.2f;
+
+    void Update() {
+        transform.Translate(Vector3.right * 4 * Time.deltaTime * direction);
+
+        gameObject.transform.position = new Vector3(
+            Mathf.Clamp(gameObject.transform.position.x, MIN_X, MAX_X),
+            gameObject.transform.position.y,
+            gameObject.transform.position.z
+        );
+    }
+}
